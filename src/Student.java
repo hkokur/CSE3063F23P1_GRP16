dcbc30f8d2246e97e1e2c6312abdc2782c4ece86
barisgirayakman <girayakman@gmail.com>
2023-12-15 00:00:33 +0300

    merged some changes

--- src/Student.java
+++ src/Student.java
@@ -6,21 +6,19 @@ public class Student extends Person {
     private String phoneNumber;
     private int semester;
     private int entranceYear;
-<<<<<<< HEAD
     private String status;
-    private ArrayList<Course> courses;
+    private ArrayList<Course> selectedCourses;
     private Transcript transcript;
 
     public Student(String personName, String personSurname, String username, String password,
             String address, String phoneNumber, int studentYear, int entranceYear, String status,
-            ArrayList<Course> courses,
+            ArrayList<Course> selectedCourses,
             Transcript transcript) {
         super(personName, personSurname, username, password);
         this.address = address;
         this.phoneNumber = phoneNumber;
-        this.studentYear = studentYear;
         this.entranceYear = entranceYear;
-        this.courses = courses;
+        this.selectedCourses = selectedCourses;
         this.transcript = transcript;
         this.status = status;
     }
@@ -39,12 +37,6 @@ public class Student extends Person {
         System.out.println(transcript.getGrades());
     }
 
-=======
-    private String status; // waiting, approved, rejected, finished, available
-    private ArrayList<Course> selectedCourses;
-    private Transcript transcript;
-
->>>>>>> 7f49d5ed85e4b624f6ddc60902887c9f8b6eafef
     public String getAddress() {
         return this.address;
     }
@@ -77,13 +69,8 @@ public class Student extends Person {
         this.entranceYear = entranceYear;
     }
 
-<<<<<<< HEAD
-    public boolean getStatus() {
-        return this.getStatus();
-=======
     public String getStatus() {
         return this.status;
->>>>>>> 7f49d5ed85e4b624f6ddc60902887c9f8b6eafef
     }
 
     public void setStatus(String status) {
@@ -91,27 +78,19 @@ public class Student extends Person {
     }
 
     public ArrayList<Course> getSelectedCourses() {
-<<<<<<< HEAD
-        return courses;
+        return selectedCourses;
     }
 
     public void setCourses(ArrayList<Course> courses) {
-        this.courses = courses;
-    }
-
-    public void clearCourses() {
-        this.courses = new ArrayList<Course>();
+        this.selectedCourses = courses;
     }
 
     public String getFullName() {
         return this.getPersonName() + " " + this.getPersonSurname();
-=======
-        return this.selectedCourses;
     }
 
     public void setSelectedCourses(ArrayList<Course> selectedCourses) {
         this.selectedCourses = selectedCourses;
->>>>>>> 7f49d5ed85e4b624f6ddc60902887c9f8b6eafef
     }
 
     public Transcript getTranscript() {
@@ -122,12 +101,10 @@ public class Student extends Person {
         this.transcript = transcript;
     }
 
-<<<<<<< HEAD
     public void addCourse(Course course){
-        this.courses.add(course);
+        this.selectedCourses.add(course);
 
     }
-
 
     // Function for checking if the student has already added the course.
     public boolean checkStudentAlreadyAddedTheCourse(Course course) {
@@ -176,57 +153,13 @@ public class Student extends Person {
 
         // If the student's year is suitable for the course, return true.
         // If the student's year is not suitable for the course, return false.
-        if (this.getStudentYear() >= courseYear) {
+        if (this.getEntranceYear() >= courseYear) {
             return true;
         } else {
-=======
-    public Student(String name, String surname, String id, String password, String address, String phoneNumber,
-            int semester, int entranceYear, String status, ArrayList<Course> selectedCourses, Transcript transcript) {
-        super(name, surname, id, password);
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.semester = semester;
-        this.entranceYear = entranceYear;
-        this.status = status;
-        this.selectedCourses = selectedCourses;
-        this.transcript = transcript;
-    }
-
-    public Student() {
-        super();
-        this.address = "";
-        this.phoneNumber = "";
-        this.semester = 0;
-        this.entranceYear = 0;
-        this.status = "";
-        this.selectedCourses = new ArrayList<Course>();
-        this.transcript = new Transcript();
-    }
-
-    // Adding a course to the selected courses of the student
-    public boolean addCourse(Course course) {
-        try {
-            selectedCourses.add(course);
-            return true;
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-            return false;
-        }
-
-    }
-
-    public boolean dropCourse(Course course) {
-        try {
-            selectedCourses.remove(course);
-            return true;
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
->>>>>>> 7f49d5ed85e4b624f6ddc60902887c9f8b6eafef
             return false;
         }
     }
 
-<<<<<<< HEAD
     // Function for parsing the year from the course short name.
     public int parseYearFromShortName(String courseShortName) {
         // Find first number in string
@@ -273,97 +206,10 @@ public class Student extends Person {
     }
     public int getTotalCredit(){
         int sum = 0;
-        for(int i = 0;i<courses.size();i++){
-            sum+=courses.get(i).getCredit();
+        for(int i = 0;i<selectedCourses.size();i++){
+            sum+=selectedCourses.get(i).getCredit();
         }
         return sum;
     }   
 
-=======
-    public ArrayList<Course> getAvailableCourses() {
-        ArrayList<Course> availableCourses = new ArrayList<Course>();
-
-        ArrayList<Course> allCourseSections = getAllCourses();
-        for (Course course : allCourseSections) {
-            if (checkStudentAlreadySelectedCourse(course) == false &&
-                    checkPrerequisite(course) == true &&
-                    checkSemesterOfCourse(course) == true) {
-                availableCourses.add(course);
-            }
-        }
-
-        return availableCourses;
-    }
-
-    public ArrayList<Course> getAllCourses() {
-
-        Json json = new Json();
-        ArrayList<CourseSection> allCourseSections = json.readCourseSections();
-        ArrayList<Course> allCourses = new ArrayList<Course>();
-
-        allCourses.addAll(allCourseSections);
-        return allCourses;
-    }
-
-    public String getWarnings() {
-
-        return "";
-    }
-
-    public int getTotalCredit() {
-
-        return 0;
-    }
-
-    public boolean checkCredits() {
-        return false;
-    }
-
-    public boolean checkStudentAlreadySelectedCourse(Course course) {
-        return false;
-    }
-
-    public boolean checkPrerequisite(Course course) {
-        return false;
-    }
-
-    public boolean checkSemesterOfCourse(Course course) {
-        return false;
-    }
-
-    // Returns true if the course is in the transcript(student has taken the course
-    // before and passed)
-    private boolean checkCourseInTranscript(Course course) {
-        for (Grade grade : transcript.getGradeList()) {
-            if (grade.getCourse() == course) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public boolean sendCoursesForApproval() {
-        return false;
-    }
-
-    public ArrayList<ArrayList<Course>> checkOverlappingCourses() {
-        return new ArrayList<ArrayList<Course>>();
-    }
-
-    // + addCourse(course: Course): boolean
-    // + dropCourse(course: Course): boolean
-    // - dropCourse(course: Course): boolean
-    // + getSelectedCourse():
-    // + getAvailableCourses():
-    // + getAllCourses():
-    // + getWarnings(): String
-    // - getTotalCredit(): int
-    // - checkCredits(): boolean
-    // - checkStudentAlreadySelectedCourse(course: Course): boolean
-    // - checkPrerequisite(course: Course): boolean
-    // - checkSemesterOfCourse(course: Course): boolean
-    // + sendCoursesForApproval(): boolean
-    // + checkOver1apingCourses(): ArrayList<ArrayList<Course>>
->>>>>>> 7f49d5ed85e4b624f6ddc60902887c9f8b6eafef
 }
